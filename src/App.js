6ea1c1cb77a19c08a624cbd4a099fab7a50a496e
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-19 23:15:36 -0600

    buttons test deleted

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ function App() {
   return (
     <div className="calculator-container">
       <CustomButton typeButton="number" label="1" />
-      <CustomButton typeButton="number" label="2" />
-      <CustomButton typeButton="number" label="3" />
-      <CustomButton typeButton="number" label="4" />
-      <CustomButton typeButton="number" label="5" />
-      <CustomButton typeButton="number" label="6" />
-      <CustomButton typeButton="number" label="7" />
-      <CustomButton typeButton="number" label="8" />
-      <CustomButton typeButton="number" label="9" />
-      <CustomButton typeButton="number" label="0" />
     </div>
   );
 }
